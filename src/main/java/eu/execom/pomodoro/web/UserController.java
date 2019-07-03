16c44121e47d100cb8bb21c8e4042bda64fc2a8f
majaraknic <37179193+majaraknic@users.noreply.github.com>
2019-07-03 16:22:07 +0200

    Registration with validation

--- src/main/java/eu/execom/pomodoro/web/UserController.java
+++ src/main/java/eu/execom/pomodoro/web/UserController.java
@@ -9,14 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.context.request.WebRequest;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -69,13 +68,6 @@ public class UserController {
     public ResponseEntity delete(@PathVariable Long id) {
         userService.delete(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
-    }
-
-    @GetMapping
-    public String registrationForm(WebRequest request, Model model) {
-        UserRegistrationDto userDto = new UserRegistrationDto();
-        model.addAttribute("user", userDto);
-        return "registration";
     }
 
 }
