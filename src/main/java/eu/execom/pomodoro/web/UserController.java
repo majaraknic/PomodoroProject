08ec8cf328535a43c18d38aaa466f0386bcf3900
majaraknic <37179193+majaraknic@users.noreply.github.com>
2019-07-04 10:44:28 +0200

    Added generic frontend with angular

--- src/main/java/eu/execom/pomodoro/web/UserController.java
+++ src/main/java/eu/execom/pomodoro/web/UserController.java
@@ -15,7 +15,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 
-@CrossOrigin(origins = "http://localhost:4200")
+//@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/user")
 public class UserController {
