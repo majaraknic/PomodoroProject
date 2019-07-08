bdb7877873067b5bc9ebff5a867bec903af5fa97
majaraknic <37179193+majaraknic@users.noreply.github.com>
2019-07-08 11:40:01 +0200

    Added Exceptions in UserService

--- src/main/java/eu/execom/pomodoro/service/UserService.java
+++ src/main/java/eu/execom/pomodoro/service/UserService.java
@@ -14,22 +14,34 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private User savedUser = new User();
+
     public List<User> getAll() {
         return userRepository.findAll();
     }
 
     public User getById(Long id) {
+        if (!userRepository.existsById(id)) {
+            throw new EntityExistsException("User with this id doesn't exist in database.");
+        }
+
         return userRepository.getOne(id);
     }
 
     public User save(User user) {
-        User savedUser = userRepository.save(user);
+
+        try {
+            savedUser = userRepository.save(user);
+        } catch (EntityExistsException e){
+            System.out.println("This entity already exists in database.");
+        }
         return savedUser;
     }
 
     public void delete(Long id) {
-       if (!userRepository.existsById(id)) {
-           throw new EntityExistsException("User with this id doesn't exist in database.");
+        if (!userRepository.existsById(id)) {
+            throw new EntityExistsException("User with this id doesn't exist in database.");
         }
         userRepository.deleteById(id);
     }
